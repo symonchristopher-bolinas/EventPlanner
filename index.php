1bace0c16f4b2bf0566c96abb4a997062b1e3cd9
symonchristopher-bolinas <angelessymon@gmail.com>
2025-04-27 00:57:29 +0800

    new file:   aboutus.php 	new file:   account/admin_dashboard.php 	renamed:    client_dashboard.php -> account/client_dashboard.php 	renamed:    login.php -> account/login.php 	renamed:    logout.php -> account/logout.php 	renamed:    process_login.php -> account/process_login.php 	renamed:    process_signup.php -> account/process_signup.php 	deleted:    admin_dashboard.php 	new file:   img/homebg.jpg 	new file:   index.php 	new file:   style/aboutus.css 	modified:   style/adminstyle.css 	new file:   style/dashboard_style.css

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+// index.php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>EventSync</title>
+    <link rel="stylesheet" href="style/adminstyle.css">
+</head>
+<body>
+    <header class="navbar">
+        <div class="logo">Event<span style="color:blue;">Sync</span></div>
+        <nav>
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="account/login.php">Sign In</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="hero">
+        <div class="overlay">
+            <h1>WELCOME TO <span style="color:black;">Event</span><span style="color:blue;">Sync</span></h1>
+            <p>LET START A PLAN</p>
+            <div class="buttons">
+                <a href="#" class="btn propose">PROPOSE PLAN</a>
+                <a href="#" class="btn read">Read more</a>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
